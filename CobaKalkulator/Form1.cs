ac98806e102f37330f52cf5fb6a6f137ba52e3e4
044_Khariza Azmi <72921762+KharizaAzmi@users.noreply.github.com>
2023-02-28 13:50:22 +0700

    Kalkulator Sederhana

---
+++ CobaKalkulator/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CobaKalkulator
+{
+    public partial class Form1 : Form
+    {
+        private string currentCalculation = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            currentCalculation += (sender as Button).Text;
+
+            textBox1.Text = currentCalculation;
+        }
+
+        private void button_Equals_Click(object sender, EventArgs e)
+        {
+            string formattedCalculation = currentCalculation.ToString().Replace("x", "*").ToString().Replace("&divide;", "/");
+
+            try
+            {
+                textBox1.Text = new DataTable().Compute(formattedCalculation, null).ToString();
+
+            }
+            catch (Exception ex)
+            {
+                textBox1.Text = "0";
+                currentCalculation = "";
+            }
+        }
+
+        private void button_Clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            currentCalculation = "";
+        }
+
+        private void button_ClearEntry_Click(object sender, EventArgs e)
+        {
+            if (currentCalculation.Length > 0)
+            {
+                currentCalculation = currentCalculation.Remove(currentCalculation.Length - 1, 1);
+            }
+            textBox1.Text = currentCalculation;
+        }
+    }
+}
